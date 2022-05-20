c34f7d22c74c121fe4875675d61611d02e97d753
Oumar <foumar404@gmail.com>
2022-05-20 12:10:12 +0200

    add to project

---
+++ lib/widegets/widgets.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: const Center(
+        child: Text('hello'),
+      ),
+    );
+  }
+}
